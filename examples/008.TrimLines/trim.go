7d8a4410cedeaccb6ad3fd4f580de221d1c8a2c9
lostjared <jaredbruni@gmail.com>
2017-07-06 16:48:15 -0700

    added trim

---
+++ examples/008.TrimLines/trim.go
@@ -0,0 +1,42 @@
+/* Program reads file, and removes empty lines then outputs to stdout */
+
+package main
+
+import(
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func checkPrint(value string) {
+	check := false
+	for _, i  := range(value)  {
+		if i != ' ' && i != '\t' && i != '\n' {
+			check = true
+		}
+	}
+	if check == true {
+		fmt.Println(value)
+	}
+}
+
+func main() {
+	fmt.Println("Trim Lines v1.0 - Jared\n\n")
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stdin, "Requires one argument of text file")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+
+	if err != nil {
+		fmt.Fprintln(os.Stdin, "Error could not open file: ", err)
+		os.Exit(1)
+	}
+
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		checkPrint(input.Text())
+	}
+	f.Close()
+}
