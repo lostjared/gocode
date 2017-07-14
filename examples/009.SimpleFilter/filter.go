7e8be151b580ad339e07d89ff614340bb8c71da3
lostjared <jaredbruni@gmail.com>
2017-07-14 15:57:51 -0700

    formatting

--- examples/009.SimpleFilter/filter.go
+++ examples/009.SimpleFilter/filter.go
@@ -18,13 +18,11 @@ func procPixel(col color.Color, alpha float32) color.Color {
 	gf += (alpha*gf)
 	bf += (alpha*bf)
 	return color.RGBA{uint8(rf), uint8(gf), uint8(bf), uint8(a)}
-
 }
 
 func FilterImage(im image.Image, alpha float32) image.Image {
 	s := im.Bounds()
 	nw := image.NewRGBA(image.Rect(0,0,s.Max.X, s.Max.Y))
-
 	for x := 0; x < s.Max.X; x++ {
 		for y := 0; y < s.Max.Y; y++ {
 			col := im.At(x, y)
@@ -34,7 +32,6 @@ func FilterImage(im image.Image, alpha float32) image.Image {
 	}
 	return nw
 }
-
 
 func main() {
 	fmt.Println("Test Filter v1.0\nWritten by Jared Bruni")
@@ -48,19 +45,15 @@ func main() {
 		os.Exit(1)
 	}
 	ifn, err := os.Open(os.Args[1])
-
 	if (err != nil) {
 		fmt.Fprintln(os.Stderr, "Error could not open file: ", err)
 		os.Exit(1)
 	}
-
 	alpha, err := strconv.ParseFloat(os.Args[3], 10)
-
 	if (err != nil) {
 		fmt.Fprintln(os.Stderr, "Error converting to float: ", err)
 		os.Exit(1)
 	}
-
 	img, err := png.Decode(ifn)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error could not open file: ", err)
