ea6d06664fed60f600e4ef9f713bd6a7ec83e750
lostjared <jaredbruni@gmail.com>
2017-07-28 17:00:11 -0700

    added ex

---
+++ features/strings/split.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"io/ioutil"
+)
+
+func readAndTokenize(source string) []string {
+	fptr, err := os.Open(source)
+	if(err != nil) {
+		fmt.Fprintln(os.Stderr, "Error could not open: ", source, " Error: ", err)
+		os.Exit(1)
+	}
+
+	s, err := ioutil.ReadAll(fptr)
+	if(err != nil) {
+		fmt.Fprintln(os.Stderr, "Error could not read file: ", source, " Error: ", err)
+	}
+	fptr.Close()
+	return strings.Split(string(s), " ")
+}
+
+
+func main() {
+	if (len(os.Args) != 2) {
+		fmt.Fprintln(os.Stderr, "Error requires one argument of the file to open.\n")
+		os.Exit(1)
+	}
+	s := readAndTokenize(os.Args[1])
+	fmt.Println("Tokens sep by space")
+	index := 1
+	for _, i := range(s) {
+		fmt.Println(index, ": ", i)
+		index ++
+	}
+	os.Exit(0);
+}
